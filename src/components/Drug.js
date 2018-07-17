85314a42b31cd71c66c680e8fdf800c350af6490
coloro <colorolabs@gmail.com>
2018-07-17 11:17:03 -0400

    dispensar button

--- src/components/Drug.js
+++ src/components/Drug.js
@@ -15,7 +15,12 @@ export default class Drug extends Component {
   render() {
     if (this.state.loading) return <li className="list-group-item"><i className="fas fa-circle-notch fa-spin fa-2x"></i></li>
     return (
-      <li className="list-group-item"><strong>{this.state.dci}</strong>, {this.props.dose} {this.state.forma} cada {this.props.frequency} por {this.props.length} Dias.</li>
+      <li className="list-group-item d-flex justify-content-between align-items-center">
+        <div>
+          <strong>{this.state.dci}</strong>, {this.props.dose} {this.state.forma} cada {this.props.frequency} por {this.props.length} Dias.
+        </div>
+        <button type="button" className="btn btn-danger btn-sm">Dispensar</button>
+      </li>
     )
   }
 }
